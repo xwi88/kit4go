c85aa278270701a6bb665f07ba3e3b0b8f4d1b6e
xwi88 <278810732@qq.com>
2020-06-22 18:53:35 +0800

    add consumer test

--- README.md
+++ README.md
@@ -18,3 +18,10 @@ go get -u github.com/xwi88/kit4go
 * [x] aerospike client
 * [x] kafka producer: async & sync producer
 * [x] kafka consumer: consumer & consumerGroup
+
+## issue
+
+- panic: non-positive interval for NewTicker
+    - `sarama-cluster@v2.1.15+incompatible/consumer.go`
+    - 1. `config.Consumer.Offsets.CommitInterval = config.Consumer.Offsets.AutoCommit.Interval`
+    - 2. kafka `github.com/Shopify/sarama v1.26.4` -> `github.com/Shopify/sarama v1.24.1`
--- kafka/consumer_test.go
+++ kafka/consumer_test.go
@@ -1,11 +1,12 @@
 package kafka
 
 import (
-	"context"
+	"encoding/json"
 	"testing"
 	"time"
 
 	"github.com/Shopify/sarama"
+	cluster "github.com/bsm/sarama-cluster"
 	"github.com/kdpujie/log4go"
 )
 
@@ -14,14 +15,16 @@ func TestConsumerConsume(t *testing.T) {
 	w.SetColor(true)
 	log4go.Register(w)
 
-	config := sarama.NewConfig()
+	config := cluster.NewConfig()
 	config.Version = sarama.V2_5_0_0 // specify appropriate version
 	config.Consumer.Return.Errors = true
-	config.Consumer.IsolationLevel = sarama.ReadCommitted // default ReadUncommitted=0
+	// config.Consumer.IsolationLevel = sarama.ReadCommitted // default ReadUncommitted=0
 	// config.Consumer.Offsets.Initial = sarama.OffsetOldest // default OffsetOldest=-2
-	config.Consumer.Offsets.Initial = sarama.OffsetNewest // default OffsetOldest=-2
-	// config.Consumer.Group.Rebalance.Timeout = time.Second*60 // default 60s
-	// config.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategyRange      // default BalanceStrategyRange
+	// config.Consumer.Offsets.Initial = sarama.OffsetNewest // default OffsetOldest=-2
+	config.Consumer.Group.Rebalance.Timeout = time.Second * 60 // default 60s
+	// config.Consumer.Offsets.CommitInterval = time.Second // FIXME: must set
+	config.Consumer.Offsets.CommitInterval = config.Consumer.Offsets.AutoCommit.Interval // FIXME: must set
+	config.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategyRange               // default BalanceStrategyRange
 	// config.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategyRoundRobin // default BalanceStrategyRange
 	config.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategySticky // default BalanceStrategyRange
 	config.Consumer.Group.Rebalance.Timeout = time.Second * 5
@@ -29,6 +32,7 @@ func TestConsumerConsume(t *testing.T) {
 	config.Consumer.Group.Rebalance.Retry.Backoff = time.Second * 2
 	config.Consumer.Group.Heartbeat.Interval = time.Second * 6
 	config.Consumer.Group.Session.Timeout = time.Second * 30
+	config.Consumer.Offsets.AutoCommit.Enable = false
 
 	brokers := []string{
 		"10.14.41.57:9092",
@@ -38,36 +42,31 @@ func TestConsumerConsume(t *testing.T) {
 	topics := []string{"d-application-sys-log"}
 	groupID := "dsp_will_20200601"
 
-	group, err := NewConsumerGroup(brokers, topics, groupID, config)
+	group, err := NewConsumer(brokers, topics, groupID, config)
 	if err != nil {
 		panic(err)
 	}
-	// defer func() { _ = group.Close() }() // error ...
+	defer func() { _ = group.Close() }() // error ...
 
 	// Track errors
-	// go func() {
-	// 	for err := range group.cg.Errors() {
-	// 		log4go.Info("[TestConsumerGroupConsume] consume errors:%v, type:%T, addr:%p", err, err, err)
-	// 	}
-	// }()
+	go func() {
+		for err := range group.c.Errors() {
+			log4go.Info("[TestConsumerGroupConsume] consume errors:%v, type:%T, addr:%p", err, err, err)
+		}
+	}()
 
 	// Iterate over consumer sessions.
-	ctx := context.Background()
+	go group.StartConsumer(testConsumer)
+	// time.Sleep(time.Second * 30)
+	select {}
 
-	// single run
-	handler := exampleConsumerGroupHandler{}
-	go group.StartConsumer(ctx, handler)
-	time.Sleep(time.Second * 60)
-	_ = group.Close()
-	time.Sleep(time.Second * 5)
+}
 
-	// forbidden infinite loop called
-	// for {
-	// 	handler := exampleConsumerGroupHandler{}
-	// 	// `Consume` should be called inside an infinite loop, when a
-	// 	// server-side rebalance happens, the consumer session will need to be
-	// 	// recreated to get the new claims
-	// 	group.StartConsumer(ctx, handler)
-	// 	// group.StartConsumer(ctx, nil) // also deal for test, but you shall not input the nil
-	// }
+func testConsumer(msg *sarama.ConsumerMessage) {
+	data := make(map[string]interface{})
+	_ = json.Unmarshal(msg.Value, &data)
+	log4go.Debug("[TestConsumerGroupConsume] testConsumer, topic:%v, partition:%v, offset:%v, timestamp:%v, "+
+		"value:%+v",
+		msg.Topic, msg.Partition, msg.Offset, msg.Timestamp, data)
+	time.Sleep(time.Second * 1)
 }
